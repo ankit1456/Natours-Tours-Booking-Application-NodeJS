bd300c5b215c79a9c60706c3dce93c119713427c
Ankit Tripathi <ankittripathi1456@email.com>
2021-10-01 16:18:44 +0530

    implemented stripe webhooks

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -47,12 +47,12 @@ exports.deleteBooking = factory.deleteOne(Booking);
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
-
 exports.webhookCheckout = (req, res, next) => {
   const signature = req.headers['stripe-signature'];
+
   let event;
   try {
     event = stripe.webhooks.constructEvent(
@@ -61,9 +61,10 @@ exports.webhookCheckout = (req, res, next) => {
       process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
-    return res.status(400).send(`Webhook error : ${err.message}`);
+    return res.status(400).send(`Webhook error: ${err.message}`);
   }
-  if (event.type === 'checkout.session.complete')
+
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
